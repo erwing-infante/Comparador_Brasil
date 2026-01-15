89a00b1bac226f353bd6e0c537a330f594cba338
Erwing Infante <erwing.infante@gmail.com>
2026-01-14 21:32:17 -0500

    ganga

--- equivalencias_equipos.py
+++ equivalencias_equipos.py
@@ -108,7 +108,7 @@ EQUIVALENCIAS_EQUIPOS = {
     "cf pachuca": "pachuca",
     "club tijuana de caliente": "xolos de tijuana",
     "fc juarez": "juarez",
-    "Deportivo Toluca": "toluca",
+    "deportivo toluca": "toluca",
     "pumas unam": "pumas unam",
     "tigres uanl": "tigres uanl",
     "aj auxerre": "auxerre",
