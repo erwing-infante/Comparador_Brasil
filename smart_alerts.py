127c8afcbe588ce68dc7fed860e1ebd2900b7cf9
erwing-infante <erwing.infante@gmail.com>
2026-03-13 20:22:05 -0500

    sacar a cesar

--- smart_alerts.py
+++ smart_alerts.py
@@ -16,7 +16,6 @@ TELEGRAM_TOKEN = os.getenv("SMART_BOT_TOKEN")
 CHAT_IDS = [
     int(os.getenv("SMART_BOT_CHAT_ID_1")),
     int(os.getenv("SMART_BOT_CHAT_ID_2")),
-    int(os.getenv("SMART_BOT_CHAT_ID_3")),
 ]
 
 UMBRAL_JUGADOR = -1.50  # -1.25%
